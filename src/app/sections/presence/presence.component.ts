329151fe560f42c182e6a35235b09b7662be0a7a
Kaptein_Sabeltann <emilnyb@hotmail.com>
2018-08-03 00:43:18 +0200

    updated to release version of August 2018

--- src/app/sections/presence/presence.component.ts
+++ src/app/sections/presence/presence.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-presence',
   templateUrl: './presence.component.html',
   styleUrls: ['./presence.component.css']
 })
-export class PresenceComponent implements OnInit {
+export class PresenceComponent {
 
-  constructor() { }
+  @ViewChild('audioToggler') audioToggler: ElementRef;
+  audioToggle: boolean = true;
 
-  ngOnInit() {
+  constructor(private renderer: Renderer2) {}
+
+  toggleAudio() {
+    if(this.audioToggle) {
+      this.renderer.removeClass(this.audioToggler.nativeElement, "fa-volume-up");
+      this.renderer.addClass(this.audioToggler.nativeElement, "fa-volume-off");
+    }
+    else {
+      this.renderer.removeClass(this.audioToggler.nativeElement, "fa-volume-off");
+      this.renderer.addClass(this.audioToggler.nativeElement, "fa-volume-up");
+    }
+
+    this.audioToggle = !this.audioToggle;
+    
+    // apply volume control
   }
-
 }
